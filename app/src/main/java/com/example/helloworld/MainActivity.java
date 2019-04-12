e1474f48eda17ba071a629d369919dbf398b80a6
Srinivas Koripella <talkwithsrinivas@yahoo.co.in>
2019-04-12 17:02:23 +0530

    Fixed checkstyle to info

--- app/src/main/java/com/example/helloworld/MainActivity.java
+++ app/src/main/java/com/example/helloworld/MainActivity.java
@@ -27,7 +27,7 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-           super.onCreate(savedInstanceState);
+         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Log.d("MainActivity", "Hello world");
 
